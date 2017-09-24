de29b22b6655a31cad1f2ad294bfa18be1936c75
JanithK93 <janithkularathne@gmail.com>
2017-09-24 21:39:28 +0530

    added changes to sensor reading classes

---
+++ AgroResearch/getHumiTempReadings.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+def getTempHumReadings():
+
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	#GPIO.cleanup()
+
+	# read data using pin 4
+	instance = dht11.DHT11(pin=4)
+
+	result = instance.read()
+	if result.is_valid():
+		arrayReadings = [result.temperature, result.humidity]
+		return  arrayReadings
+		GPIO.cleanup()
+	else:
+		print("invalid")
+
--- AgroResearch/growthPrediction/growthANN.py
+++ AgroResearch/growthPrediction/growthANN.py
@@ -1,163 +1,207 @@
 import numpy as pckg
 import time
+import pygame
+
+#from ..growthPrediction import play_audio as play
+
+def growthPredicion (ldrValue, temp, humidity):
+    currentTime = time.strftime("%H %M")
+
+    # assign parameter values
+    lightReading = ldrValue
+    temperatureReading = temp
+    humidityReading = humidity
+
+    # Mean value for the growth
+    # Sample values are given for the testing purposes
+    averageGrowth = 0.0001
+    averageHumidity = 80
+    averageTemp= 24
+    averageLight = 5
+
+    # keep the record about the amount of harmful enviroment changes occur
+    effectCount = 0
+    effectCountLight = 0
+    effectCountHumidity = 0
+    effectCountTemp = 0
+
+    effectCountLightLow = 0
+    effectCountHumidityLow = 0
+    effectCountTempLow = 0
 
 
-currentTime = time.strftime("%H %M")
+    # Run the application in give periods
+    #if currentTime== "08 00" or currentTime== "11 00" or currentTime== "13 00" or currentTime== "16 00" or currentTime== "19 00" or currentTime== "22 00" or currentTime== "01 00" \
+           # or currentTime == "04 00" :
 
-# Mean value for the growth
-# Sample values are given for the testing purposes
-averageGrowth = 0.0001
-averageHumidity = 80
-averageTemp= 24
-averageLight = 5
+    if currentTime == currentTime: # just for the testing purposes
 
-# keep the record about the amount of harmful enviroment changes occur
-effectCount = 0
-effectCountLight = 0
-effectCountHumidity = 0
-effectCountTemp = 0
+        def nonlinear(facts, derive = False):
+            if(derive == True):
+                return facts*(1-facts)
 
-effectCountLightLow = 0
-effectCountHumidityLow = 0
-effectCountTempLow = 0
+            else:
+                return 1/(1+pckg.exp(-facts))
 
+        # Readings of the sensors are give as input using the array
+        # sunlight, temperature, humidity respectively
+        facts = pckg.array([ [5.40,28,81],
+                           [6.20,29,81.2],
+                           [6.59,27.5,81.6],
+                           [5.87,28,82] ])
 
-# Run the application in give periods
-#if currentTime== "08 00" or currentTime== "11 00" or currentTime== "13 00" or currentTime== "16 00" or currentTime== "19 00" or currentTime== "22 00" or currentTime== "01 00" \
-       # or currentTime == "04 00" :
+        # Expected output after processing
+        # Values taken by monitoring growth of plants
 
-if currentTime == currentTime: # just for the testing purposes
+        output = pckg.array([ [0.001029],
+                              [0.001531],
+                              [0.001100],
+                              [0.001011] ])
 
-    def nonlinear(facts, derive = False):
-        if(derive == True):
-            return facts*(1-facts)
+        pckg.random.seed(1)
 
-        else:
-            return 1/(1+pckg.exp(-facts))
+        # Initialize weights randomly with a mean of 0
+        weight1 = 2*pckg.random.random((3,4))-1
+        weight2 = 2*pckg.random.random((4,1))-1
 
-    # Readings of the sensors are give as input using the array
-    # sunlight, temperature, humidity respectively
-    facts = pckg.array([ [5.40,28,81],
-                       [6.20,29,81.2],
-                       [6.59,27.5,81.6],
-                       [5.87,28,82] ])
+        # feed forward for all layers
+        for i in range(600000):
 
-    # Expected output after processing
-    # Values taken by monitoring growth of plants
+            layer0 = facts;
+            layer1 = nonlinear(pckg.dot(layer0,weight1))
+            layer2 = nonlinear(pckg.dot(layer1,weight2))
 
-    output = pckg.array([ [0.001029],
-                          [0.001531],
-                          [0.001100],
-                          [0.001011] ])
+            # Size of the error
+            # Difference between real value and predicted value
+            layer2Error = output-layer2
 
-    pckg.random.seed(1)
+            if (layer2Error [0]) != 0 and (layer2Error[0]) >= 0.000000000001:
+                # Print the amount of error when it satisfies the condition
 
-    # Initialize weights randomly with a mean of 0
-    weight1 = 2*pckg.random.random((3,4))-1
-    weight2 = 2*pckg.random.random((4,1))-1
+                if(i % 100000) == 0:
+                    print ("Error" + str(pckg.mean(pckg.abs(layer2Error))))
 
-    # feed forward for all layers
-    for i in xrange(600000):
+                layer2Value = layer2Error*nonlinear(layer2,derive = True)
 
-        layer0 = facts;
-        layer1 = nonlinear(pckg.dot(layer0,weight1))
-        layer2 = nonlinear(pckg.dot(layer1,weight2))
-
-        # Size of the error
-        # Difference between real value and predicted value
-        layer2Error = output-layer2
-
-        if (layer2Error [0]) != 0 and (layer2Error[0]) >= 0.000000000001:
-            # Print the amount of error when it satisfies the condition
-
-            if(i % 100000) == 0:
-                print "Error" + str(pckg.mean(pckg.abs(layer2Error)))
-
-            layer2Value = layer2Error*nonlinear(layer2,derive = True)
-
-            # How much layer1 values affected to layer2 error with respect to te weights
-            layer1Error = layer2Value.dot(weight2.T)
+                # How much layer1 values affected to layer2 error with respect to te weights
+                layer1Error = layer2Value.dot(weight2.T)
 
 
-            layer1Value = layer1Error*nonlinear(layer1, derive = True)
+                layer1Value = layer1Error*nonlinear(layer1, derive = True)
 
-            weight2 += layer1.T.dot(layer2Value)
-            weight1 += layer0.T.dot(layer1Value)
+                weight2 += layer1.T.dot(layer2Value)
+                weight1 += layer0.T.dot(layer1Value)
 
-    print layer2
+        print (layer2)
 
-    # if predicted value is not greater than the average growth keep a record by increasing effectCount
-    if layer2[1] < averageGrowth:
-        effectCount += 1
+        # if predicted value is not greater than the average growth keep a record by increasing effectCount
+        if layer2[1] < averageGrowth:
+            effectCount += 1
 
-        # if effecCount is equal or greater than 3 checks which factor causes the effect to make the warning
-        if effectCount >= 3:
-            light = facts[0]
-            temp = facts[1]
-            humi = facts[2]
+            # if effecCount is equal or greater than 3 checks which factor causes the effect to make the warning
+            if effectCount >= 3:
+                light = facts[0]
+                temp = facts[1]
+                humi = facts[2]
 
-            # keep records about each factor
-            if light > averageLight:
-                effectCountLight += 1
+                # keep records about each factor
+                if light > averageLight:
+                    effectCountLight += 1
 
-                # sound the alarm
-                if effectCountLight >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountLight=0
-                    effectCount=0
+                    # sound the alarm
+                    if effectCountLight >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Sun_light_intensity_is_high.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
 
-            elif light < averageLight:
-                effectCountLightLow += 1
+                        effectCountLight=0
+                        effectCount=0
 
-                # sound the alarm
-                if effectCountLightLow >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountLight = 0
-                    effectCount = 0
+                elif light < averageLight:
+                    effectCountLightLow += 1
 
-            if  temp > averageTemp:
-                effectCountTemp += 1
+                    # sound the alarm
+                    if effectCountLightLow >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Sun_light_intensity_is_low.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
 
-                if effectCountTemp >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountTemp = 0
-                    effectCount = 0
+                        effectCountLight = 0
+                        effectCount = 0
 
-            elif temp < averageHumidity:
-                effectCountTempLow += 1
+                if  temp > averageTemp:
+                    effectCountTemp += 1
 
-                if effectCountTempLow >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountTemp = 0
-                    effectCount = 0
+                    if effectCountTemp >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Temperature_is_high.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
 
-            if humi > averageHumidity:
-                effectCountHumidity += 1
+                        effectCountTemp = 0
+                        effectCount = 0
 
-                if effectCountHumidity >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountHumidity = 0
-                    effectCount = 0
+                elif temp < averageTemp:
+                    effectCountTempLow += 1
 
-            elif humi < averageHumidity:
-                effectCountHumidityLow += 1
+                    if effectCountTempLow >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Temperature_is_Low.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
 
-                if effectCountHumidityLow >= 3:
-                    #
-                    # Sound the alarm as necessary
-                    #
-                    effectCountHumidity = 0
-                    effectCount = 0
+                        effectCountTemp = 0
+                        effectCount = 0
+
+                if humi > averageHumidity:
+                    effectCountHumidity += 1
+
+                    if effectCountHumidity >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Humidity_is_high.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
+
+                        effectCountHumidity = 0
+                        effectCount = 0
+
+                elif humi < averageHumidity:
+                    effectCountHumidityLow += 1
+
+                    if effectCountHumidityLow >= 3:
+                        #
+                        # Sound the alarm as necessary
+                        #
+                        pygame.mixer.init()
+                        pygame.mixer.music.load("../audioFile/Humidity_is_low.wav")
+                        pygame.mixer.music.play()
+                        while pygame.mixer.music.get_busy() == True:
+                            continue
+
+                        effectCountHumidity = 0
+                        effectCount = 0
 
 
 
