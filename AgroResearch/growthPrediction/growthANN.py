c6ef31e624353f12ded6f8234975c440ba46d3aa
JanithK93 <janithkularathne@gmail.com>
2017-09-27 00:23:06 +0530

    removed the errors occurred in the previous version of code

--- AgroResearch/growthPrediction/growthANN.py
+++ AgroResearch/growthPrediction/growthANN.py
@@ -92,7 +92,7 @@ def growthPredicion (ldrValue, temp, humidity):
                 weight2 += layer1.T.dot(layer2Value)
                 weight1 += layer0.T.dot(layer1Value)
 
-        print ("layer twoo  ", layer2)
+        print ("Predicted Value:  ", layer2)
 
         # if predicted value is not greater than the average growth keep a record by increasing effectCount
         '''if (layer2[0] < averageGrowth) or (layer2[0] > averageGrowth):
