c4f1cfee8b282ec1be40f0373a55db07d4040a53
JanithK93 <janithkularathne@gmail.com>
2017-11-20 10:11:23 +0530

    removed the errors occurred in the previous version of code

--- AgroResearch/growthPrediction/calculateAge.py
+++ AgroResearch/growthPrediction/calculateAge.py
@@ -1,10 +1,13 @@
-import datetime
+import os
 import pygame
 
+def func():
+    pygame.mixer.init()
+    pygame.mixer.music.load(wavpath)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
 
-pygame.mixer.init()
-pygame.mixer.music.load("../audioFile/Humidity_is_high.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+wavpath = os.path.abspath('../audioFile/Humidity_is_high.wav')
+func()
 
