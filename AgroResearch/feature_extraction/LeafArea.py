a433415cbef4b14b508a5b35c2abafa4dfbe8127
bliss93 <sithumiamarasinghe93@gmail.com>
2017-11-29 11:28:37 +0530

    Change the code Leaf Area detection

--- AgroResearch/feature_extraction/LeafArea.py
+++ AgroResearch/feature_extraction/LeafArea.py
@@ -3,104 +3,101 @@ import numpy as np
 import glob
 from matplotlib import pyplot as plt
 
-#for img in glob.glob('C:/Users/User/PycharmProjects/researchP/Normal_L/*.jpg'):
+#for img in glob.glob('C:/Users/User/PycharmProjects/Last_29/Normal/*.jpg'):
+def leafArea(image):
 
-def  leafArea (image):
+    img0 = image
+    imgRead0 = cv2.imread(img0, 1)
 
-    imgbu = image
 
-    imgReadbu = cv2.imread(imgbu, 1)
 
-    cv2.namedWindow('resized_w01', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('resized_w01', 400, 550)
 
-    zbu = imgReadbu.reshape((-1, 3))
-    zbu = np.float32(zbu)
+    z0 = imgRead0.reshape((-1, 3))
+    z0 = np.float32(z0)
 
-    criteriabu = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 10.0)
-    Kbu = 4
-    retbu, labelbu, centerbu = cv2.kmeans(zbu, Kbu, None, criteriabu, 10, cv2.KMEANS_RANDOM_CENTERS)
+    criteria0 = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 20.0)
+    K0 = 3
+    ret0, label0, center0 = cv2.kmeans(z0, K0, None, criteria0, 20, cv2.KMEANS_RANDOM_CENTERS)
 
-    centerbu = np.uint8(centerbu)
+    center0 = np.uint8(center0)
 
-    resbu = centerbu[labelbu.flatten()]
+    res0 = center0[label0.flatten()]
 
-    res01bu = resbu.reshape((imgReadbu.shape))
+    res010 = res0.reshape((imgRead0.shape))
 
-    imgraybu = cv2.cvtColor(res01bu, cv2.COLOR_BGR2GRAY)
+
+
+
+    blur0 = cv2.blur(imgRead0, (3, 3))
+
+
+
+    #range for green
+    l_green0 = np.array([36, 0, 0])
+    u_green0 = np.array([86,255,255])
+
+    thresh = cv2.inRange(blur0, l_green0, u_green0)
+
+    res_mask0 = cv2.bitwise_and(blur0, blur0, mask=thresh)
+
+    thresh20 = thresh.copy()
+
+
+
+
 
     # erosion
-    kernel01bu = np.ones((5, 5), np.uint8)
-    erosion01bu = cv2.erode(imgraybu, kernel01bu, iterations=1)
-
-    retbu, th_graybu = cv2.threshold(erosion01bu, 80, 255, 0)
-
-    bit_andImgbu = cv2.bitwise_and(res01bu, res01bu, mask=th_graybu)
-
-    _, cntsbu, _ = cv2.findContours(th_graybu, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(cntsbu) != 0:
-        #        cv2.drawContours(bit_andImg,cnts,-1,[0,0,255],10)
-
-        max_areabu = max(cntsbu, key=cv2.contourArea)
-
-        cv2.drawContours(bit_andImgbu, max_areabu, -1, [0, 0, 255], 10)
-
-        # Contour Approximation
-        epsilonbu = 0.1 * cv2.arcLength(max_areabu, True)
-        approxbu = cv2.approxPolyDP(max_areabu, epsilonbu, True)
-
-        areabu = cv2.contourArea(max_areabu)
-
-        # print  area
-
-        print 'detected area in the spot -->>  ' + str(areabu)
-
-      # # print 'detected area in the  spot -->>  ' + str(np.countNonZero(max_area))
-      #
-      # equa_diameter = np.sqrt(4 * areabu/ np.pi)
-      #
-      # # print 'detected diameter in the spot -->>  ' + str(equa_diameter)
-      #
-      # # print 'detected average colour of spot -->>  ' + str(area.mean())
+    kernel010 = np.ones((5, 5), np.uint8)
+    erosion010 = cv2.erode(thresh, kernel010, iterations=1)
 
 
 
 
 
-        # print '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    #bit_andImg = cv2.bitwise_and(res01, res01, mask=thresh)
 
-        #   cv2.imshow('resized_w01', bit_andImg)
-        #   cv2.waitKey(5000)
-        #   cv2.destroyAllWindows()
-        #
-        #  print approx
-        #  print '**************************************'
-
-    ## Show-image
-    #numpy_horizontl = np.hstack((imgRead, bit_andImg))
-    #numpy_horizontl_conct = np.concatenate((imgRead, bit_andImg), axis=1)
-    #
-    #cv2.namedWindow('resized_w01', cv2.WINDOW_NORMAL)
-    #cv2.resizeWindow('resized_w01', 800, 550)
-    #
-    #cv2.imshow('resized_w01', numpy_horizontl_conct)
-    #cv2.waitKey(5000)
-    #cv2.destroyAllWindows()
+    _, cnts0, _ = cv2.findContours(erosion010, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 
-    #Show_image
-        #cv2.namedWindow('resized_w01', cv2.WINDOW_NORMAL)
-        #cv2.resizeWindow('resized_w01', 400, 450)
-        #
-        #cv2.namedWindow('resized_w02', cv2.WINDOW_NORMAL)
-        #cv2.resizeWindow('resized_w02', 400, 450)
-        #
-        ##cv2.namedWindow('resized_w03', cv2.WINDOW_NORMAL)
-        ##cv2.resizeWindow('resized_w03', 400, 450)
-        #
-        #cv2.imshow('resized_w01', imgReadbu)
-        #cv2.imshow('resized_w02', res_maskSpotsbu)
-        ##cv2.imshow('resized_w03', res_maskGreen2)
-        #cv2.waitKey(500000)
-        #cv2.destroyAllWindows()
+
+    # finding contour with maximum area and store it as best_cnt
+    max_area = 0
+    best_cnt0 = 1
+    for cnt in cnts0:
+        area0 = cv2.contourArea(cnt)
+        if area0 > max_area:
+            max_area = area0
+            best_cnt0 = cnt
+
+
+    print 'Green area detected -->>' + str(max_area)
+
+
+
+
+   ## finding centroids of best_cnt and draw a circle there
+   #M = cv2.moments(best_cnt)
+   #cx, cy = int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])
+   ## if best_cnt>1:
+   #cv2.circle(res01, (cx, cy), 10, (0, 0, 255), -1)
+
+
+    cv2.drawContours(res_mask0,best_cnt0,-1,[0,0,255],10)
+
+
+
+    # Show_image
+#cv2.namedWindow('resized_w01', cv2.WINDOW_NORMAL)
+#cv2.resizeWindow('resized_w01', 400, 450)
+#
+#cv2.namedWindow('resized_w02', cv2.WINDOW_NORMAL)
+#cv2.resizeWindow('resized_w02', 400, 450)
+#
+#
+#cv2.imshow('resized_w01', res_mask0)
+#cv2.imshow('resized_w02', imgRead0)
+#
+#
+#cv2.waitKey(50000)
+#cv2.destroyAllWindows()
+#
