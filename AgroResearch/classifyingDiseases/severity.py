d11d65de9018805c065e075c90b07e9342eb0cb3
Toshiba <peshanimadavika@gmail.com>
2017-12-02 04:35:45 +0530

    updated

--- AgroResearch/classifyingDiseases/severity.py
+++ AgroResearch/classifyingDiseases/severity.py
@@ -4,29 +4,27 @@ import cv2
 from pygame.locals import *
 
 
-diseased_area = 10
-green_area = 20
 
+def severityPred(averageSpotArea,leafArea):
 
+    if(averageSpotArea > leafArea):
+        pygame.mixer.init()
+        pygame.mixer.music.load("audioFile/Severity_is_high.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
 
-if(diseased_area > green_area):
-    pygame.mixer.init()
-    pygame.mixer.music.load("audioFile/Severity_is_high.wav")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    elif(averageSpotArea <  leafArea):
+        pygame.mixer.init()
+        pygame.mixer.music.load("audioFile/Severity_is_low.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
 
-elif(diseased_area  <  green_area):
-    pygame.mixer.init()
-    pygame.mixer.music.load("audioFile/Severity_is_low.wav")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-
-else:
-    pygame.mixer.init()
-    pygame.mixer.music.load("audioFile/Severity_is_moderate.wav")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    else:
+        pygame.mixer.init()
+        pygame.mixer.music.load("audioFile/Severity_is_moderate.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
 
